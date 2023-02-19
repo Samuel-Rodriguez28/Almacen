5ccfe3eba7e42e3020922d088e8419b52855ea86
Samuel Rodriguez <samrdzfls28@gmail.com>
2023-02-19 15:25:35 -0600

    Cambiada la direccion del paquete de la ventana para que no saliera error

--- src/com/almacen/mx/view/aplicaciondepartamentomantenimiento/Ventana.java
+++ src/com/almacen/mx/view/aplicaciondepartamentomantenimiento/Ventana.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package aplicaciondepartamentomantenimiento;
+package com.almacen.mx.view.aplicaciondepartamentomantenimiento;
 
 import aplicaciondepartamentomantenimiento.customUI.CustomHeader;
 import aplicaciondepartamentomantenimiento.customUI.ModeloTabla;
