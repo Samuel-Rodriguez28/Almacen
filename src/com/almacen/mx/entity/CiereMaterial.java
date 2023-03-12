2b72a61597da27b31b4f40854cfedcd2e566c4d8
jaher <jaher@DESKTOP-NBNNACQ>
2023-03-11 23:27:50 -0600

    Se agrego las entidades a trrabajar

---
+++ src/com/almacen/mx/entity/CiereMaterial.java
@@ -0,0 +1,126 @@
+
+package com.almacen.mx.entity;
+
+/**
+ *
+ * @author jaher
+ */
+public class CiereMaterial {
+
+    private int idCierrematerial;
+    private int oc;
+    private int numReq;
+    private int numFac;
+    private String proveedor;
+    private String fechaSolicitud;
+    private String fechaEntrada;
+    private String nombreArticulo;
+    private String observacion;
+
+    
+    public CiereMaterial() {
+    }
+
+    public CiereMaterial(int idCierrematerial, int oc, int numReq, int numFac, String proveedor, 
+                        String fechaSolicitud, String fechaEntrada, String nombreArticulo, String observacion) {
+        this.idCierrematerial = idCierrematerial;
+        this.oc = oc;
+        this.numReq = numReq;
+        this.numFac = numFac;
+        this.proveedor = proveedor;
+        this.fechaSolicitud = fechaSolicitud;
+        this.fechaEntrada = fechaEntrada;
+        this.nombreArticulo = nombreArticulo;
+        this.observacion = observacion;
+    }
+
+    public int getIdCierrematerial() {
+        return idCierrematerial;
+    }
+
+    public void setIdCierrematerial(int idCierrematerial) {
+        this.idCierrematerial = idCierrematerial;
+    }
+
+    public int getOc() {
+        return oc;
+    }
+
+    public void setOc(int oc) {
+        this.oc = oc;
+    }
+
+    public int getNumReq() {
+        return numReq;
+    }
+
+    public void setNumReq(int numReq) {
+        this.numReq = numReq;
+    }
+
+    public int getNumFac() {
+        return numFac;
+    }
+
+    public void setNumFac(int numFac) {
+        this.numFac = numFac;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public String getFechaSolicitud() {
+        return fechaSolicitud;
+    }
+
+    public void setFechaSolicitud(String fechaSolicitud) {
+        this.fechaSolicitud = fechaSolicitud;
+    }
+
+    public String getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public void setFechaEntrada(String fechaEntrada) {
+        this.fechaEntrada = fechaEntrada;
+    }
+
+    public String getNombreArticulo() {
+        return nombreArticulo;
+    }
+
+    public void setNombreArticulo(String nombreArticulo) {
+        this.nombreArticulo = nombreArticulo;
+    }
+
+    public String getObservacion() {
+        return observacion;
+    }
+
+    public void setObservacion(String observacion) {
+        this.observacion = observacion;
+    }
+
+    @Override
+    public String toString() {
+        return "CiereMaterial"
+                + "id Cierrematerial; " + idCierrematerial + "\n"
+                + " oc; " + oc + "\n"
+                + " Numero de Reqistro; " + numReq + "\n"
+                + " Numero de Factor; " + numFac + "\n"
+                + " proveedor; " + proveedor + "\n"
+                + " Fecha Solicitud; " + fechaSolicitud + "\n"
+                + " Fecha Entrada; " + fechaEntrada + "\n"
+                + " Nombre Articulo; " + nombreArticulo + "\n"
+                + " Observacion; " + observacion ;
+    }
+    
+    
+    
+    
+}
--- src/com/almacen/mx/entity/Stock.java
+++ src/com/almacen/mx/entity/Stock.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.almacen.mx.entity;
 
 /**
@@ -9,5 +6,106 @@ package com.almacen.mx.entity;
  * @author Samuel
  */
 public class Stock {
+    private int idStock;
+    private int unidades;
+    private int codigo;
+    private String nombreArticulo;
+    private String marca;
+    private String observacion;
+    private int minimo;
+    private String medida;
+
+    public Stock() {
+    }
+
+    public Stock(int idSTOCK, int unidades, int codigo, String nombreArticulo, String marca, String observacion, int minimo, String medida) {
+        this.idStock = idSTOCK;
+        this.unidades = unidades;
+        this.codigo = codigo;
+        this.nombreArticulo = nombreArticulo;
+        this.marca = marca;
+        this.observacion = observacion;
+        this.minimo = minimo;
+        this.medida = medida;
+    }
+
+    public int getIdStock() {
+        return idStock;
+    }
+
+    public void setIdStock(int idStock) {
+        this.idStock = idStock;
+    }
+
+    public int getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(int unidades) {
+        this.unidades = unidades;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombreArticulo() {
+        return nombreArticulo;
+    }
+
+    public void setNombreArticulo(String nombreArticulo) {
+        this.nombreArticulo = nombreArticulo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getObservacion() {
+        return observacion;
+    }
+
+    public void setObservacion(String observacion) {
+        this.observacion = observacion;
+    }
+
+    public int getMinimo() {
+        return minimo;
+    }
+
+    public void setMinimo(int minimo) {
+        this.minimo = minimo;
+    }
+
+    public String getMedida() {
+        return medida;
+    }
+
+    public void setMedida(String medida) {
+        this.medida = medida;
+    }
+
+    @Override
+    public String toString() {
+        return "Stock "  
+                        + " id STOCK; " + idStock + "\n"
+                        + " Unidades; " + unidades + "\n"
+                        + " Codigo; " + codigo + "\n"
+                        + " Nombre de Articulo; " + nombreArticulo + "\n"
+                        + " Marca; " + marca + "\n"
+                        + " Observacion; " + observacion + "\n"
+                        + " Minimo; " + minimo + "\n"
+                        + " Medida; " + medida ;
+    }
+    
+    
     
 }
