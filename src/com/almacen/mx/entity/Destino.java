c246bf97fd2c884f3a62585f297dbe6cecb7aba8
Smith Hdz <Smitherdez@homtail.com>
2023-07-02 20:25:07 -0600

    entidades

--- src/com/almacen/mx/entity/Destino.java
+++ src/com/almacen/mx/entity/Destino.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.almacen.mx.entity;
 
 /**
  *
- * @author jaher
+ * @author SmithHDZ
  */
 public class Destino {
+    private int idDestino ;
+    private String nombre;
+    
+    public Destino (){
+    }
+    
+    public Destino (int idDestino, String nombre){
+    this.idDestino =idDestino ;
+    this.nombre=nombre;
+    }
+
+    public int getIdDestino() {
+        return idDestino;
+    }
+
+    public void setIdDestino(int idDestino) {
+        this.idDestino = idDestino;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Destino{" + "idDestino=" + idDestino + ", nombre=" + nombre + '}';
+    }
+    
     
 }
